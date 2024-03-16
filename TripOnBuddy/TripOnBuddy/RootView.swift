3ce33643414a7aae1e462d8e474b75742ceccd6f
Sunil Sharma <77779035+soulofadreamer@users.noreply.github.com>
2024-03-17 00:30:50 +0530

    add initial views

--- TripOnBuddy/TripOnBuddy/RootView.swift
+++ TripOnBuddy/TripOnBuddy/RootView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,5 +21,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RootView()
 }
---
+++ TripOnBuddy/TripOnBuddy/Views/MySpaceView.swift
@@ -0,0 +1,18 @@
+//
+//  MySpaceView.swift
+//  TripOnBuddy
+//
+//  Created by Sunil Sharma on 17/03/24.
+//
+
+import SwiftUI
+
+struct MySpaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MySpaceView()
+}
