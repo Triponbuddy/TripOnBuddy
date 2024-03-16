9f80fb2e5916e20de727cea27544b878923075b5
Triponbuddy <163658800+Triponbuddy@users.noreply.github.com>
2024-03-16 23:58:33 +0530

    Add files via upload
    initial code

---
+++ TripOnBuddy/TripOnBuddy/TripOnBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripOnBuddyApp.swift
+//  TripOnBuddy
+//
+//  Created by Sunil Sharma on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TripOnBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
