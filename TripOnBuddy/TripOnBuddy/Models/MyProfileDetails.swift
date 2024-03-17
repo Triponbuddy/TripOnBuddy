ba5026b183b6a85d48220fd6e51b35ddc3d7f8ab
Sunil Sharma <77779035+soulofadreamer@users.noreply.github.com>
2024-03-18 00:59:58 +0530

    add profile details

--- TripOnBuddy/TripOnBuddy/Models/MyProfileDetails.swift
+++ TripOnBuddy/TripOnBuddy/Models/MyProfileDetails.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct MyProfileDetails: Identifiable {
+    let id = UUID()
+    var name: String
+    var userImage: String?
+    var bio: String?
+    var profession: String?
+    var gender: String?
+   
+//    init(name: String, userImage: String? = nil, bio: String? = nil, profession: String? = nil, gender: String? = nil, emailId: String, phoneNumber: [LoginDetails]) {
+//        self.name = name
+//        self.userImage = userImage
+//        self.bio = bio
+//        self.profession = profession
+//        self.gender = gender
+//        self.emailId = emailId
+//        self.phoneNumber = phoneNumber
+//    }
+    
+}
