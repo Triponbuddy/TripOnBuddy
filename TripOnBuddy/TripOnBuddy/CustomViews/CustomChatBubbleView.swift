a8382e33ad95e9962e9563e09ef4bf39a281ba92
Sunil Sharma <77779035+soulofadreamer@users.noreply.github.com>
2024-06-15 23:29:42 -0600

    Single Chat view ready!

---
+++ TripOnBuddy/TripOnBuddy/CustomViews/CustomChatBubbleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomChatBubbleView.swift
+//  TripOnBuddy
+//
+//  Created by Sunil Sharma on 03/06/24.
+//
+
+import SwiftUI
+
+struct CustomChatBubbleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomChatBubbleView()
+}
---
+++ TripOnBuddy/TripOnBuddy/Views/Title Bar Views/Chat Views/SingleChatView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleChatView.swift
+//  TripOnBuddy
+//
+//  Created by Sunil Sharma on 03/06/24.
+//
+
+import SwiftUI
+
+struct SingleChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleChatView()
+}
